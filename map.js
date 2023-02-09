3e28c260920dd5b5d6d058450d813c89a8724c04
mrludovicc <nadal46@hotmail.com>
2023-02-09 17:28:13 -0500

    Adding map assignment -Didn't know what I'm actually doing-

---
+++ map.js
@@ -0,0 +1,30 @@
+const eqArrays = (arr1, arr2) => {
+  let statement = true;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      statement = false
+    }
+  } return statement
+}
+const assertArrayEqual = (array, callback) => {
+  if (eqArrays(array, callback)) {
+    console.log(`✅✅✅ Assertion Passed: ${array} === ${callback}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array} !== ${callback}`)
+  }
+}
+const words = ["ground", "control", "to", "major", "tom"];
+
+// const results1 = words.map(word => word[0]);
+// console.log(results1);
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
