8d6dd2cf253c7cc17dcf70aebbf79e241f677b72
mrludovicc <nadal46@hotmail.com>
2023-02-12 21:53:10 -0500

    Adding assert equal and assert equal arrays(checking if the result === expected) to letter position assignment

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,22 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } for (let item of arr1) {
+    if (arr1[item] !== arr2[item]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+  } else if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+  }
+};
+
 let letterPosition = (str) => {
   let string = str.toLowerCase().split(' ').join('');
   let letters = {};
@@ -7,7 +26,8 @@ let letterPosition = (str) => {
     } else {
       letters[string[i]].push(i);
     }
-  } console.log(letters)
+  }
+  return letters;
 
 };
-letterPosition('Hello There amigo')
+assertArraysEqual(letterPosition('ARIGATOOOO AMIGOS').o, [6, 7, 8, 9, 14]);
