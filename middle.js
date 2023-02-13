5137bc887effd775bb2b9c42ea212b21e7e022c9
mrludovicc <nadal46@hotmail.com>
2023-02-12 21:57:45 -0500

    Adding assert equal (checking function) to map assingment

--- middle.js
+++ middle.js
@@ -32,6 +32,6 @@ const middle = function (array) {
   }
 }
 
-console.log(middle([4, 2, 5, 4, 4, 6, 7, 7, 4, 5]));
-console.log(middle([4, 2, 5]));
+assertArrayEqual(middle([4, 2, 5, 4, 4, 6, 7, 7, 4, 5]), [6, 4]);
+assertArrayEqual(middle([4, 2, 5]), [2]);
 
