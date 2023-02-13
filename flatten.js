a71522529b19641710b496636d7b3ed58481c81c
mrludovicc <nadal46@hotmail.com>
2023-02-12 21:20:21 -0500

    Adding flatten assignment using recursion Week-1 Day-7

---
+++ flatten.js
@@ -0,0 +1,30 @@
+const eqArrays = (result, expected) => {
+  let statement = true;
+  if (result.length !== expected.length) return false;
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== expected[i]) {
+      statement = false
+    }
+  } return statement
+}
+
+const assertArrayEqual = (result, expected) => {
+  if (eqArrays(result, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${result} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${result} !== ${expected}`)
+  }
+}
+const flatten = (arr) => {
+  let result = [];
+  let expected = [1, 2, 3, 4, 5, 6]
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      result = result.concat(flatten(element))
+    } else {
+      result.push(element)
+    }
+  }
+  return result;
+}
+assertArrayEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
