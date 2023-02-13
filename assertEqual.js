53744e839de7575f4318d29527c660ac21276f2d
mrludovicc <nadal46@hotmail.com>
2023-02-13 18:22:11 -0500

    Adding refactored assertEqual and test file

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function (actual, expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
