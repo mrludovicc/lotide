166376b3ccd150b6242f99983e18b0798b28b72d
mrludovicc <nadal46@hotmail.com>
2023-02-13 18:42:36 -0500

    Adding mocha and chai packages and changing head and middle test files to mocha style

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
 const middle = require('../middle')
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual')
-assertArraysEqual(middle([4, 2, 5, 4, 4, 6, 7, 7, 4, 5]), [6, 4]);
-assertArraysEqual(middle([4, 2, 5]), [2]);
+
+
+describe("#middle", () => {
+  it("returns [6, 4] for [4, 2, 5, 4, 4, 6, 7, 7, 4, 5])", () => {
+    assert.equal(assertArraysEqual(middle([4, 2, 5, 4, 4, 6, 7, 7, 4, 5]), [6, 4]));
+  });
+
+  it("returns [4, 2, 5]) for [2]", () => {
+    assert.equal(assertArraysEqual(middle([4, 2, 5]), [2]));
+  });
+});
+// assertArraysEqual(middle([4, 2, 5, 4, 4, 6, 7, 7, 4, 5]), [6, 4]);
+// assertArraysEqual(middle([4, 2, 5]), [2]);
 
