6fc62c3aa5fe57e4156b6b6619ad8b5c1ac75905
mrludovicc <nadal46@hotmail.com>
2023-02-13 19:26:28 -0500

    Adding mocha and chai testing to eqArrays and adding index file (objects)

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays')
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays
+};
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,25 @@
 const assertEqual = require('../assertEqual')
 const eqArrays = require('../eqArrays')
+const assert = require('chai').assert
+describe("#eqArrays", () => {
+  it('returns true for [1, 2, 3], [1, 2, 3]', () => {
+    assert.deepEqual(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+  });
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+  it('returns false for [1, 2, 3], [3, 2, 1]', () => {
+    assert.deepEqual(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+  });
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+  it('returns true for [5, 6, 9, 11, 12, 25], [5, 6, 9, 11, 12, 25]', () => {
+    assert.deepEqual(assertEqual(eqArrays([5, 6, 9, 11, 12, 25], [5, 6, 9, 11, 12, 25]), true));
+  });
+
+});
+
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
